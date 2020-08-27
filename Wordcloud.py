cc738ee884766e49900067514e96aa1acacfe413
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2020-08-27 16:41:30 -0500

    Test netx graphml

--- Wordcloud.py
+++ Wordcloud.py
@@ -7,5 +7,21 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 import matplotlib.pyplot as plt
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-text = open(path.join(d, 'sen.txt')).read()
+text = open(path.join(d, 'sen.txt'),encoding='utf-8').read()
 wordcloud = WordCloud().generate(text)
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+
+# lower max_font_size
+wordcloud = WordCloud(max_font_size=40).generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import itertools
 import numpy as np
 import pandas as pd
 import os
 
-G = nx.Graph()
+
 
 dirName = 'redes'
 
@@ -16,22 +17,7 @@ if not os.path.exists(dirName):
 else:
     print("Carpeta ", dirName, " ya existe")
 
-dirName = 'tempDir2/temp2/temp'
-file = "c.xlsx"
-x1 = pd.ExcelFile(file)
-
-#print(xl.sheet_names)
-df1 = x1.parse('Impro')
-
-Items = ["A1", "A2", "B", "C", "CLA", "CLA1", "CLB", "LA", "LA1", "LB", "SPIMO", "PI", "PMOU", "PON", "EC", "CAR", "CC",
-         "SR", "SOFT", "PP", "DI", "CCM", "CCD", "CM", "CD", "CCP", "VEP", "VED", "VEM", "FEP", "FEM", "FED", "JI",
-         "PN", "PIN"]
-item = ['Artículo A1']
-articulos_A1 = df1.loc[(df1['SUBPRODUCTO'] == "Artículo A1")]
-
-
-
-print (articulos_A1.columns)
-print (articulos_A1.count())
-
-
+G = nx.read_graphml('InvTest.graphml')
+nx.draw(G)
+print(G.order())
+plt.show()
