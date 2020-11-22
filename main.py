23bf1ee4970d0e848b987c64df282c54432ab4e6
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2020-11-22 18:31:53 -0500

    iNICIO COMPATIBILIDAD CON pYsCIENTI

--- main.py
+++ main.py
@@ -3,21 +3,56 @@ import matplotlib.pyplot as plt
 import itertools
 import numpy as np
 import pandas as pd
+from pandas import *
 import os
 
+G = nx.Graph()
 
 
-dirName = 'redes'
+excel_origen = ExcelFile('NGG.xlsx')
+df = excel_origen.parse(excel_origen.sheet_names[2])
 
+def explode(df, lst_cols, fill_value='', preserve_index=False):
+    # make sure `lst_cols` is list-alike
+    if (lst_cols is not None
+        and len(lst_cols) > 0
+        and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
+        lst_cols = [lst_cols]
+    # all columns except `lst_cols`
+    idx_cols = df.columns.difference(lst_cols)
+    # calculate lengths of lists
+    lens = df[lst_cols[0]].str.len()
+    # preserve original index values
+    idx = np.repeat(df.index.values, lens)
+    # create "exploded" DF
+    res = (pd.DataFrame({
+                col:np.repeat(df[col].values, lens)
+                for col in idx_cols},
+                index=idx)
+             .assign(**{col:np.concatenate(df.loc[lens>0, col].values)
+                            for col in lst_cols}))
+    # append those rows that have empty lists
+    if (lens == 0).any():
+        # at least one list in cells is empty
+        res = (res.append(df.loc[lens==0, idx_cols], sort=False)
+                  .fillna(fill_value))
+    # revert the original index order
+    res = res.sort_index()
+    # reset index if requested
+    if not preserve_index:
+        res = res.reset_index(drop=True)
+    return res
 
-    # Crea carpeta si no existe
-if not os.path.exists(dirName):
-    os.mkdir(dirName)
-    print("Carpeta ", dirName, " creada")
-else:
-    print("Carpeta ", dirName, " ya existe")
+df2 = explode(df.assign(Autores=df.Autores.str.split(',')), 'Autores')
 
-G = nx.read_graphml('InvTest.graphml')
-nx.draw(G)
-print(G.order())
-plt.show()
+df3 = df2 [['Titulo', 'Autores']]
+
+print(df3)
+
+G = nx.from_pandas_edgelist(df3, 'Autores', 'Titulo')
+
+# from matplotlib.pyplot import figure
+# figure(figsize=(10, 8))
+# nx.draw_shell(G, with_labels=True)
+
+print(G.nodes)
