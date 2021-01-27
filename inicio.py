c6d3f2a0d5c24bc5695350b11c1ae4fc30d9fc08
Jaime Velez <javelezea@gmail.com>
2021-01-27 10:53:03 -0500

    implementacion inicial pyqt

---
+++ inicio.py
@@ -0,0 +1,18 @@
+from erevna_ui import *
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.salir)
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
