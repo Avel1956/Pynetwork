15b4681fa1d2c95feff853d83302711ba19ec71c
javel <javelezea@gmail.com>
2021-01-27 16:42:13 -0500

    UI seleccion de archivos y procesamiento

--- inicio.py
+++ inicio.py
@@ -1,15 +1,50 @@
 from erevna_ui import *
+from main import *
+import tkinter as tk
+from tkinter import filedialog
+import networkx as nx
+import matplotlib.pyplot as plt
+import itertools as iter
+import numpy as np
+import pandas as pd
+from pandas import *
 
-
+# Clase principal de la interfaz
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
 
-        self.pushButton.clicked.connect(self.salir)
+        # conector boton de salida
+        self.pushExit.clicked.connect(self.closeIt)
+        self.actionLoad.triggered.connect(self.openFile)
+        self.actionExec.triggered.connect(self.processFile)
+
+    # definicion para el cierre de la ventana
+    def closeIt(self):
+        self.close()
+    # def actionLoad(self):
+
+    def openFile(self, df):
+        class Investigador:
+            def __init__(self, df):
+                self.df = df
+        root = tk.Tk()
+        root.withdraw()
+
+        file_path = filedialog.askopenfilename()
+        excel_origen = ExcelFile(file_path)
+        df = excel_origen.parse(excel_origen.sheet_names[2])
+        a = Investigador(df)
+        print(file_path)
+        initialNetwork(a.df)
 
 
 
+
+    def processFile(self, df):
+        df = a.df
+        initialNetwork(a)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
--- main.py
+++ main.py
@@ -6,75 +6,76 @@ import pandas as pd
 from pandas import *
 import os
 # iniciar graph
-G = nx.Graph()
-
-# importar archivo excel
-excel_origen = ExcelFile('NGG.xlsx')
-# convertir en dataframe la hoja con la informacion de articulos y autores
-df = excel_origen.parse(excel_origen.sheet_names[2])
-
-# expandir los autores separados con comas en la hoja original
-def explode(df, lst_cols, fill_value='', preserve_index=False):
-    # make sure `lst_cols` is list-alike
-    if (lst_cols is not None
-        and len(lst_cols) > 0
-        and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
-        lst_cols = [lst_cols]
-    # all columns except `lst_cols`
-    idx_cols = df.columns.difference(lst_cols)
-    # calculate lengths of lists
-    lens = df[lst_cols[0]].str.len()
-    # preserve original index values
-    idx = np.repeat(df.index.values, lens)
-    # create "exploded" DF
-    res = (pd.DataFrame({
-                col:np.repeat(df[col].values, lens)
-                for col in idx_cols},
-                index=idx)
-             .assign(**{col:np.concatenate(df.loc[lens>0, col].values)
-                            for col in lst_cols}))
-    # append those rows that have empty lists
-    if (lens == 0).any():
-        # at least one list in cells is empty
-        res = (res.append(df.loc[lens==0, idx_cols], sort=False)
-                  .fillna(fill_value))
-    # revert the original index order
-    res = res.sort_index()
-    # reset index if requested
-    if not preserve_index:
-        res = res.reset_index(drop=True)
-    return res
-
-df2 = explode(df.assign(Autores=df.Autores.str.split(',')), 'Autores')
-
-# reducir el dataframe a los articulos y autores
-df3 = df2 [['Titulo', 'Autores']]
-
-# agrupar los autores por titulo de articulo
-df4 = df3.groupby('Titulo').Autores.apply(lambda x: list(iter.combinations(x, 2)))
-
-# crear un vector con las combinaciones de pares de autores para cada articulo
-a = []
-for name in df4.index:
-    a.append(df4.loc[name])
-b = []
-for sublist in a:
-    for val in sublist:
-        b.append(val)
 
 
-# añadir la informacion de las relaciones
-G.add_edges_from(b)
+def initialNetwork (dataf):
 
-# representar
-plt.subplot(121)
-nx.draw(G, with_labels=True)
-plt.show()
-print(nx.info(G))
+    G = nx.Graph()
 
-# from matplotlib.pyplot import figure
-# figure(figsize=(10, 8))
-# nx.draw_shell(G, with_labels=True)
+
+
+    # expandir los autores separados con comas en la hoja original
+    def explode(dataf, lst_cols, fill_value='', preserve_index=False):
+        # make sure `lst_cols` is list-alike
+        if (lst_cols is not None
+            and len(lst_cols) > 0
+            and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
+            lst_cols = [lst_cols]
+        # all columns except `lst_cols`
+        idx_cols = dataf.columns.difference(lst_cols)
+        # calculate lengths of lists
+        lens = dataf[lst_cols[0]].str.len()
+        # preserve original index values
+        idx = np.repeat(dataf.index.values, lens)
+        # create "exploded" DF
+        res = (pd.DataFrame({
+                    col:np.repeat(dataf[col].values, lens)
+                    for col in idx_cols},
+                    index=idx)
+                 .assign(**{col:np.concatenate(dataf.loc[lens>0, col].values)
+                                for col in lst_cols}))
+        # append those rows that have empty lists
+        if (lens == 0).any():
+            # at least one list in cells is empty
+            res = (res.append(dataf.loc[lens==0, idx_cols], sort=False)
+                      .fillna(fill_value))
+        # revert the original index order
+        res = res.sort_index()
+        # reset index if requested
+        if not preserve_index:
+            res = res.reset_index(drop=True)
+        return res
+
+    df2 = explode(dataf.assign(Autores=dataf.Autores.str.split(',')), 'Autores')
+
+    # reducir el dataframe a los articulos y autores
+    df3 = df2 [['Titulo', 'Autores']]
+
+    # agrupar los autores por titulo de articulo
+    df4 = df3.groupby('Titulo').Autores.apply(lambda x: list(iter.combinations(x, 2)))
+
+    # crear un vector con las combinaciones de pares de autores para cada articulo
+    a = []
+    for name in df4.index:
+        a.append(df4.loc[name])
+    b = []
+    for sublist in a:
+        for val in sublist:
+            b.append(val)
+
+
+    # añadir la informacion de las relaciones
+    G.add_edges_from(b)
+
+    # representar
+    plt.subplot(121)
+    nx.draw(G, with_labels=True)
+    plt.show()
+    print(nx.info(G))
+
+    # from matplotlib.pyplot import figure
+    # figure(figsize=(10, 8))
+    # nx.draw_shell(G, with_labels=True)
 
 
 
